342d07d7b2fc81eb9c4675e8e098d77f1deb9ac0
Syl <sylvielyt@gmail.com>
2024-01-13 15:41:08 +0800

    Brain_Fried

---
+++ hap.py
@@ -0,0 +1,17 @@
+ayy = 21
+print(ayy)
+list_values = [0, 1, 2, 3, 4, 5, 6, 7, ayy]
+print(list_values)
+list_haha = [1, 2, 3]
+print(list_haha[0])
+print(list_haha[1])
+print(list_haha[2])
+print(list(range(3,78)))
+for i in range (0, 10):
+    print(i)
+    print(".")
+list_people = ["sylvie", "harold", "yuanlin"]
+for i in range(0,3):
+    print(i)
+    print(list_people[i])
+list_
